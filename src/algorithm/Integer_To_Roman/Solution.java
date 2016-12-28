ba0cec8ec8c792086280898ef823ca569db0360b
hcc <doubleview@163.com>
2016-12-28 20:39:28 +0800

    :sparkles:

---
+++ src/algorithm/Integer_To_Roman/Solution.java
@@ -0,0 +1,39 @@
+package algorithm.Integer_To_Roman;
+
+
+/**
+ * Given an integer, convert it to a roman numeral.
+ * <p>
+ * Input is guaranteed to be within the range from 1 to 3999.
+ */
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(intToRoman(1));
+        System.out.println(intToRoman(11));
+        System.out.println(intToRoman(3));
+        System.out.println(intToRoman(4));
+        System.out.println(intToRoman(234));
+    }
+
+    /**
+     * 将指定数字转化为罗马数字
+     *
+     * @param num
+     * @return
+     */
+    public static String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<values.length;i++) {
+            while(num >= values[i]) {
+                num -= values[i];
+                sb.append(strs[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/algorithm/Roman_To_Integer/Solution.java
@@ -0,0 +1,56 @@
+package algorithm.Roman_To_Integer;
+
+/**
+Given a roman numeral, convert it to an integer.
+
+        Input is guaranteed to be within the range from 1 to 3999.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("I"));
+        System.out.println(romanToInt("XI"));
+        System.out.println(romanToInt("IV"));
+        System.out.println(romanToInt("CCXXXIV"));
+    }
+
+
+    public static int romanToInt(String s) {
+        int nums[]=new int[s.length()];
+        for(int i=0;i<s.length();i++){
+            switch (s.charAt(i)){
+                case 'M':
+                    nums[i]=1000;
+                    break;
+                case 'D':
+                    nums[i]=500;
+                    break;
+                case 'C':
+                    nums[i]=100;
+                    break;
+                case 'L':
+                    nums[i]=50;
+                    break;
+                case 'X' :
+                    nums[i]=10;
+                    break;
+                case 'V':
+                    nums[i]=5;
+                    break;
+                case 'I':
+                    nums[i]=1;
+                    break;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1])
+                sum-=nums[i];
+            else
+                sum+=nums[i];
+        }
+        return sum+nums[nums.length-1];
+    }
+
+}
