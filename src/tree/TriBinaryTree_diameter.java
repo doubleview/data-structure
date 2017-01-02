6818e3be652b9f3c41c3562933ca289aba54d1ac
hcc <doubleview@163.com>
2017-01-02 22:10:43 +0800

    :sparkles:

--- src/tree/TriBinaryTree_diameter.java
+++ src/tree/TriBinaryTree_diameter.java
@@ -8,7 +8,7 @@ public class TriBinaryTree_diameter {
 	public static void main(String[] args) {
 		String[] prelist = { "A", "B", "D", null, "G", null, null, null, "C",
 				"E", null, null, "F", "H" };
-		TriBinaryTree<String> bitree = new TriBinaryTree<String>(prelist);
+		TriBinaryTree<String> bitree = new TriBinaryTree<>(prelist);
 		bitree.preOrder();
 		bitree.printDiameter();// 输出二叉树的直径
 	}
