6a58f9bc8fa6b1b112b02067cf681641b1cd56e2
hcc <doubleview@163.com>
2017-01-04 21:19:38 +0800

    :sparkles:

--- src/algorithm/Remove_Nth_Node_From_End_Of_List/Solution.java
+++ src/algorithm/Remove_Nth_Node_From_End_Of_List/Solution.java
@@ -4,6 +4,16 @@ package algorithm.Remove_Nth_Node_From_End_Of_List;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Given a linked list, remove the nth node from the end of list and return its head.
+ *
+ * For example,
+
+     Given linked list: 1->2->3->4->5, and n = 2.
+
+     After removing the second node from the end, the linked list becomes 1->2->3->5.
+ *
+ */
 public class Solution {
 
     public static void main(String[] args) {
