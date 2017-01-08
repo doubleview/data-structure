950943b82657a36d9c63bda84433b770bd413e9e
hcc <doubleview@163.com>
2017-01-08 14:09:31 +0800

    :sparkles:

--- src/algorithm/Generate_Parentheses/Solution.java
+++ src/algorithm/Generate_Parentheses/Solution.java
@@ -3,6 +3,12 @@ package algorithm.Generate_Parentheses;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+        For example, given n = 3, a solution set is:
+*/
 
 public class Solution {
 
