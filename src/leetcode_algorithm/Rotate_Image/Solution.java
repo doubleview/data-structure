905c08dfa1e4f86faeb3d47f652cf9a30130c3f0
hcc <doubleview@163.com>
2017-02-06 23:04:04 +0800

    :sparkles:

---
+++ src/leetcode_algorithm/Rotate_Image/Solution.java
@@ -0,0 +1,40 @@
+package leetcode_algorithm.Rotate_Image;
+
+
+import java.util.Arrays;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        new Solution().rotate(matrix);
+        for (int[] nums : matrix) {
+         System.out.println(Arrays.toString(nums));
+        }
+    }
+
+    /**
+     * ÍÆ¼ö½â·¨
+     * @param matrix
+     */
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i< matrix.length; i++) {
+            for(int j  = i + 1; j < matrix.length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i< matrix.length; i++) {
+            for(int j  = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
+}
