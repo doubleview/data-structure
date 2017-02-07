6bce893b315cbdccf096fd1a74ca2827c9b615f7
hcc <doubleview@163.com>
2017-02-07 20:59:57 +0800

    :sparkles:

--- src/leetcode_algorithm/Rotate_Image/Solution.java
+++ src/leetcode_algorithm/Rotate_Image/Solution.java
@@ -3,8 +3,19 @@ package leetcode_algorithm.Rotate_Image;
 
 import java.util.Arrays;
 
-public class Solution {
 
+/**
+
+You are given an n x n 2D matrix representing an image.
+
+        Rotate the image by 90 degrees (clockwise).
+
+        Follow up:
+        Could you do this in-place?
+
+*/
+
+public class Solution {
 
     public static void main(String[] args) {
         int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
