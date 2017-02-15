1f4115694af6bb4b395c6a09886c31d3581e482b
hcc <doubleview@163.com>
2017-02-15 20:41:43 +0800

    :sparkles:

---
+++ src/leetcode_algorithm/Wildcard_Matching/Solution.java
@@ -0,0 +1,58 @@
+package leetcode_algorithm.Wildcard_Matching;
+
+/**
+ * Implement wildcard pattern matching with support for '?' and '*'.
+ * <p>
+ * '?' Matches any single character.
+ * '*' Matches any sequence of characters (including the empty sequence).
+ * <p>
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * The function prototype should be:
+ * bool isMatch(const char *s, const char *p)
+ * <p>
+ * Some examples:
+ * isMatch("aa","a") ¡ú false
+ * isMatch("aa","aa") ¡ú true
+ * isMatch("aaa","aa") ¡ú false
+ * isMatch("aa", "*") ¡ú true
+ * isMatch("aa", "a*") ¡ú true
+ * isMatch("ab", "?*") ¡ú true
+ * isMatch("aab", "c*a*b") ¡ú false
+ */
+
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isMatch("aa", "a"));
+        System.out.println(new Solution().isMatch("aa", "aa"));
+        System.out.println(new Solution().isMatch("aaa", "aa"));
+        System.out.println(new Solution().isMatch("aa", "*"));
+        System.out.println(new Solution().isMatch("aa", "a*"));
+        System.out.println(new Solution().isMatch("aa", "?*"));
+        System.out.println(new Solution().isMatch("aab", "c*a*b"));
+    }
+
+    public boolean isMatch(String s, String p) {
+        boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
+        match[s.length()][p.length()] = true;
+        for (int i = p.length() - 1; i >= 0; i--) {
+            if (p.charAt(i) != '*') break;
+            else match[s.length()][i] = true;
+        }
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
+                    match[i][j] = match[i + 1][j + 1];
+                else if (p.charAt(j) == '*')
+                    match[i][j] = match[i + 1][j] || match[i][j + 1];
+                else match[i][j] = false;
+            }
+        }
+        return match[0][0];
+    }
+
+}
