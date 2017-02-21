f1b103c3d01908f4ed92dd1cf35f6aba9e1bb010
hcc <doubleview@163.com>
2017-02-21 21:02:32 +0800

    :sparkles:

--- src/leetcode_algorithm/Jump_Game/Solution.java
+++ src/leetcode_algorithm/Jump_Game/Solution.java
@@ -21,6 +21,12 @@ public class Solution {
     }
 
 
+    /**
+     * 个人解法
+     *
+     * @param nums
+     * @return
+     */
     public boolean canJump(int[] nums) {
         int max = nums[0];
         for (int i = 0; i < nums.length; i++) {
