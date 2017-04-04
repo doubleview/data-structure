1e316368afd85bb7a836c4b4252443d5a5bb1b11
hcc <2426775354@qq.com>
2017-04-04 17:26:13 +0800

    :sparkles:

---
+++ src/leetcode_algorithm/q102_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,81 @@
+package leetcode_algorithm;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * return its level order traversal as:
+ * [
+ * [3],
+ * [9,20],
+ * [15,7]
+ * ]
+ */
+public class q102_BinaryTreeLevelOrderTraversal {
+
+
+    /**
+     * 解法1 利用队列
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> wrapList = new LinkedList<>();
+        if (root == null) return wrapList;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<>();
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) queue.offer(queue.peek());
+                if (queue.peek().right != null) queue.offer(queue.peek());
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+
+    /**
+     * 解法2 DFS
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        levelHelper(res, root, 0);
+        return res;
+    }
+
+    private void levelHelper(List<List<Integer>> res, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= res.size()) res.add(new LinkedList<>());
+        res.get(height).add(root.val);
+        levelHelper(res, root.left, height + 1);
+        levelHelper(res, root.right, height + 1);
+    }
+
+    private final static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
