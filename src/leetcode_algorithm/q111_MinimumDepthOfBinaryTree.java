dabefe3afc5a844bf8724f47cf96a35614d25ad2
hcc <doubleview@163.com>
2017-04-11 21:41:23 +0800

    :sparkles:

---
+++ src/leetcode_algorithm/q111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,45 @@
+package leetcode_algorithm;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * <p>
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ */
+
+public class q111_MinimumDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        t1.left = t2;
+        t1.right = t3;
+        q111_MinimumDepthOfBinaryTree solution = new q111_MinimumDepthOfBinaryTree();
+        System.out.println(solution.minDepth(t1));
+    }
+
+
+    /**
+     * 解法1 (个人解法)
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1 : Math.min(left, right) + 1;
+    }
+
+
+    private final static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
