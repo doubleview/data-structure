ded4ade1887ccc1c0b62d34ad459903efb6f154f
hcc <doubleview@163.com>
2017-04-17 20:23:34 +0800

    :sparkles:

--- src/leetcode_algorithm/q117_PopulatingNextRightPointersInEachNode2.java
+++ src/leetcode_algorithm/q117_PopulatingNextRightPointersInEachNode2.java
@@ -2,29 +2,31 @@ package leetcode_algorithm;
 
 import java.util.LinkedList;
 import java.util.Queue;
-
 /**
- * Follow up for problem "Populating Next Right Pointers in Each Node".
- * <p>
- * What if the given tree could be any binary tree? Would your previous solution still work?
- * <p>
- * Note:
- * <p>
- * You may only use constant extra space.
- * For example,
- * Given the following binary tree,
- * 1
- * /  \
- * 2    3
- * / \    \
- * 4   5    7
- * After calling your function, the tree should look like:
- * 1 -> NULL
- * /  \
- * 2 -> 3 -> NULL
- * / \    \
- * 4-> 5 -> 7 -> NULL
- */
+
+Follow up for problem "Populating Next Right Pointers in Each Node".
+
+        What if the given tree could be any binary tree? Would your previous solution still work?
+
+        Note:
+
+        You may only use constant extra space.
+        For example,
+        Given the following binary tree,
+        1
+        /  \
+        2    3
+        / \    \
+        4   5    7
+        After calling your function, the tree should look like:
+        1 -> NULL
+        /  \
+        2 -> 3 -> NULL
+        / \    \
+        4-> 5 -> 7 -> NULL
+
+*/
+
 
 public class q117_PopulatingNextRightPointersInEachNode2 {
 
